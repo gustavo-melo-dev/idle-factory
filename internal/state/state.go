a7613ce26935e7354e43d7fa792fa4d9d428c3ce
Gustavo B. <gustavobrunodemelo@gmail.com>
2025-12-23 16:47:33 -0300

    feat: make state safe for concurrency

--- internal/state/state.go
+++ internal/state/state.go
@@ -2,7 +2,9 @@ package state
 
 import (
 	"fmt"
+	"sort"
 	"strings"
+	"sync"
 
 	"github.com/gustavo-melo-dev/idle-factory/internal/machine"
 	"github.com/gustavo-melo-dev/idle-factory/internal/resource"
@@ -11,9 +13,13 @@ import (
 type State struct {
 	Machines  map[machine.MachineType][]string
 	Resources map[resource.ResourceType]float64
+	mu        sync.RWMutex // state is updated concurrently
 }
 
 func (s *State) String() string {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
 	var result strings.Builder
 	// ANSI escape codes explanation:
 	//   - \033 is the ESC character (ASCII 27). It signals the start of an escape sequence.
@@ -22,16 +28,37 @@ func (s *State) String() string {
 	// Combining these codes effectively clears the terminal screen and resets the cursor position.
 	result.WriteString("\033[2J\033[H")
 	result.WriteString("Current State:\n")
+
+	// maps in Go do not guarantee order, so we sort the resource types for consistent output
+	var resourceTypes []int
+	for resType := range s.Resources {
+		resourceTypes = append(resourceTypes, int(resType))
+	}
+	sort.Ints(resourceTypes)
+
 	result.WriteString("Resources:\n")
-	for resType, amount := range s.Resources {
+	for resourceType := range resourceTypes {
+		// convert from int back to ResourceType
+		resourceType := resource.ResourceType(resourceType)
+
 		result.WriteString("- ")
-		result.WriteString(resType.String())
+		result.WriteString(resourceType.String())
 		result.WriteString(": ")
-		result.WriteString(fmt.Sprintf("%.2f", amount))
+		result.WriteString(fmt.Sprintf("%.2f", s.Resources[resourceType]))
 		result.WriteString("\n")
 	}
+
+	var machineTypes []int
+	for mType := range s.Machines {
+		machineTypes = append(machineTypes, int(mType))
+	}
+	sort.Ints(machineTypes)
+
 	result.WriteString("Machines:\n")
 	for mType, machines := range s.Machines {
+		// convert from int back to MachineType
+		mType := machine.MachineType(mType)
+
 		result.WriteString("- ")
 		result.WriteString(mType.String())
 		result.WriteString(": ")
@@ -47,9 +74,15 @@ func New() *State {
 	}
 }
 func (s *State) UpdateMachine(mType machine.MachineType, id string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
 	s.Machines[mType] = append(s.Machines[mType], id)
 }
 
 func (s *State) UpdateResource(resType resource.ResourceType, amount float64) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
 	s.Resources[resType] += amount
 }
